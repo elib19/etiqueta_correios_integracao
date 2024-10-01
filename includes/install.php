1401239ce1a0cee1f439994d771be5dd3a8e2db7
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 20:48:13 -0300

    Update install.php

--- includes/install.php
+++ includes/install.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
 function correios_install() {
     global $wpdb;
     $table_name = $wpdb->prefix . 'correios_vendors';
@@ -6,7 +10,6 @@ function correios_install() {
     $charset_collate = $wpdb->get_charset_collate();
 
     $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
         vendor_id bigint(20) NOT NULL,
         remnom varchar(255) NOT NULL,
         remend varchar(255) NOT NULL,
@@ -14,16 +17,12 @@ function correios_install() {
         remcom varchar(255),
         rembai varchar(255) NOT NULL,
         remcid varchar(255) NOT NULL,
-        remest varchar(50) NOT NULL,
+        remest varchar(255) NOT NULL,
         remcep varchar(20) NOT NULL,
-        remcpf varchar(20),
-        PRIMARY KEY (id)
+        remcpf varchar(20) NOT NULL,
+        PRIMARY KEY (vendor_id)
     ) $charset_collate;";
 
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
     dbDelta( $sql );
-}
-
-function correios_populate_initial_data() {
-    // Popule com dados iniciais se necessário
 }
