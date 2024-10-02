a65243f29b96a53ee7c775d64734dd9817e0813d
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 23:09:20 -0300

    Update helper.php

--- includes/helper.php
+++ includes/helper.php
@@ -7,7 +7,6 @@ function correios_save_vendor_data( $vendor_id, $data ) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'correios_vendors';
 
-    // Insere ou atualiza os dados do vendedor
     $wpdb->replace(
         $table_name,
         array(
@@ -23,8 +22,4 @@ function correios_save_vendor_data( $vendor_id, $data ) {
             'remcpf'    => $data['remcpf'],
         )
     );
-}
-
-function correios_get_api_key() {
-    return get_option( 'correios_api_key' );
 }
