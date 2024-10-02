45bf3468a65c714ee6685a6252726efcfcddd4ca
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 23:42:14 -0300

    Update settings-page.php

--- views/settings-page.php
+++ views/settings-page.php
@@ -11,12 +11,18 @@ if ( ! defined( 'ABSPATH' ) ) {
         settings_fields( 'correios_options_group' );
         do_settings_sections( 'correios_settings' );
 
+        // Obter as opções armazenadas
         $api_key = get_option( 'correios_api_key' );
+        $correios_cartao = get_option( 'correios_cartao' );
         ?>
         <table class="form-table">
             <tr valign="top">
                 <th scope="row">API Key:</th>
                 <td><input type="text" name="correios_api_key" value="<?php echo esc_attr( $api_key ); ?>" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Número do Cartão Correios Fácil:</th>
+                <td><input type="text" name="correios_cartao" value="<?php echo esc_attr( $correios_cartao ); ?>" /></td>
             </tr>
         </table>
         <?php submit_button(); ?>
