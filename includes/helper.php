f4dfeaa29cd20fe3748624680d38f6113d9096e1
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 22:53:34 -0300

    Update helper.php

--- includes/helper.php
+++ includes/helper.php
@@ -7,19 +7,24 @@ function correios_save_vendor_data( $vendor_id, $data ) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'correios_vendors';
 
+    // Insere ou atualiza os dados do vendedor
     $wpdb->replace(
         $table_name,
         array(
             'vendor_id' => $vendor_id,
-            'remnom'    => sanitize_text_field( $data['remnom'] ),
-            'remend'    => sanitize_text_field( $data['remend'] ),
-            'remnum'    => sanitize_text_field( $data['remnum'] ),
-            'remcom'    => sanitize_text_field( $data['remcom'] ),
-            'rembai'    => sanitize_text_field( $data['rembai'] ),
-            'remcid'    => sanitize_text_field( $data['remcid'] ),
-            'remest'    => sanitize_text_field( $data['remest'] ),
-            'remcep'    => sanitize_text_field( $data['remcep'] ),
-            'remcpf'    => sanitize_text_field( $data['remcpf'] ),
+            'remnom'    => $data['remnom'],
+            'remend'    => $data['remend'],
+            'remnum'    => $data['remnum'],
+            'remcom'    => $data['remcom'],
+            'rembai'    => $data['rembai'],
+            'remcid'    => $data['remcid'],
+            'remest'    => $data['remest'],
+            'remcep'    => $data['remcep'],
+            'remcpf'    => $data['remcpf'],
         )
     );
+}
+
+function correios_get_api_key() {
+    return get_option( 'correios_api_key' );
 }
