aa20363fd4f9a1a25b8c087c6e18d745d296c770
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-02 00:35:20 -0300

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -2,38 +2,50 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+?>
 
-function correios_settings_page() {
-    ?>
-    <div class="wrap">
-        <h1>Configurações do Plugin Correios</h1>
-        <form method="post" action="options.php">
-            <?php
-            settings_fields( 'correios_options_group' );
-            do_settings_sections( 'correios_settings' );
-
-            $api_key = get_option( 'correios_api_key' );
-            $cartao_correios = get_option( 'correios_cartao_correios' ); // Novo campo para o número do cartão
-            ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">API Key:</th>
-                    <td><input type="text" name="correios_api_key" value="<?php echo esc_attr( $api_key ); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Número do Cartão Correios Fácil:</th>
-                    <td><input type="text" name="correios_cartao_correios" value="<?php echo esc_attr( $cartao_correios ); ?>" /></td>
-                </tr>
-            </table>
-            <?php submit_button(); ?>
-        </form>
-    </div>
-    <?php
-}
-
-add_action( 'admin_init', 'correios_settings_init' );
-
-function correios_settings_init() {
-    register_setting( 'correios_options_group', 'correios_api_key' );
-    register_setting( 'correios_options_group', 'correios_cartao_correios' ); // Registro do novo campo
-}
+<div class="wrap">
+    <h2>Dados do Remetente</h2>
+    <form method="post">
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Nome:</th>
+                <td><input type="text" id="remnom" name="remnom" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Endereço:</th>
+                <td><input type="text" id="remend" name="remend" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Número:</th>
+                <td><input type="text" id="remnum" name="remnum" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Complemento:</th>
+                <td><input type="text" id="remcom" name="remcom" class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Bairro:</th>
+                <td><input type="text" id="rembai" name="rembai" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Cidade:</th>
+                <td><input type="text" id="remcid" name="remcid" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Estado:</th>
+                <td><input type="text" id="remest" name="remest" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">CEP:</th>
+                <td><input type="text" id="remcep" name="remcep" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">CPF/CNPJ:</th>
+                <td><input type="text" id="remcpf" name="remcpf" class="regular-text"></td>
+            </tr>
+        </table>
+        
+        <?php submit_button( 'Salvar Dados' ); ?>
+    </form>
+</div>
