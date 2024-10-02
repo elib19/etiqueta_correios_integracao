daf46b748253fc12f74a39d8a4b74a72e2158948
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 23:10:04 -0300

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -3,18 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-// Adiciona menu de configurações
-function correios_add_admin_menu() {
-    add_menu_page(
-        'Configurações Correios',
-        'Correios',
-        'manage_options',
-        'correios_settings',
-        'correios_settings_page'
-    );
-}
-add_action( 'admin_menu', 'correios_add_admin_menu' );
-
 function correios_settings_page() {
     ?>
     <div class="wrap">
@@ -25,11 +13,16 @@ function correios_settings_page() {
             do_settings_sections( 'correios_settings' );
 
             $api_key = get_option( 'correios_api_key' );
+            $cartao_numero = get_option( 'correios_cartao_numero' );
             ?>
             <table class="form-table">
                 <tr valign="top">
                     <th scope="row">API Key:</th>
                     <td><input type="text" name="correios_api_key" value="<?php echo esc_attr( $api_key ); ?>" /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Número do Cartão Correios Fácil:</th>
+                    <td><input type="text" name="correios_cartao_numero" value="<?php echo esc_attr( $cartao_numero ); ?>" /></td>
                 </tr>
             </table>
             <?php submit_button(); ?>
@@ -42,4 +35,5 @@ add_action( 'admin_init', 'correios_settings_init' );
 
 function correios_settings_init() {
     register_setting( 'correios_options_group', 'correios_api_key' );
+    register_setting( 'correios_options_group', 'correios_cartao_numero' );
 }
