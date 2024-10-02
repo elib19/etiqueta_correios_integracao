a4bd4eeb655767b54bffeb172f9823223097d7fe
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-01 22:38:37 -0300

    Update vendor-data-form.php

--- views/vendor-data-form.php
+++ views/vendor-data-form.php
@@ -2,31 +2,31 @@
     <h2>Dados do Remetente</h2>
     <form method="post">
         <label for="remnom">Nome:</label>
-        <input type="text" name="remnom" id="remnom" required><br>
+        <input type="text" id="remnom" name="remnom" required>
 
         <label for="remend">Endereço:</label>
-        <input type="text" name="remend" id="remend" required><br>
+        <input type="text" id="remend" name="remend" required>
 
         <label for="remnum">Número:</label>
-        <input type="text" name="remnum" id="remnum" required><br>
+        <input type="text" id="remnum" name="remnum" required>
 
         <label for="remcom">Complemento:</label>
-        <input type="text" name="remcom" id="remcom"><br>
+        <input type="text" id="remcom" name="remcom">
 
         <label for="rembai">Bairro:</label>
-        <input type="text" name="rembai" id="rembai" required><br>
+        <input type="text" id="rembai" name="rembai" required>
 
         <label for="remcid">Cidade:</label>
-        <input type="text" name="remcid" id="remcid" required><br>
+        <input type="text" id="remcid" name="remcid" required>
 
         <label for="remest">Estado:</label>
-        <input type="text" name="remest" id="remest" required><br>
+        <input type="text" id="remest" name="remest" required>
 
         <label for="remcep">CEP:</label>
-        <input type="text" name="remcep" id="remcep" required><br>
+        <input type="text" id="remcep" name="remcep" required>
 
         <label for="remcpf">CPF/CNPJ:</label>
-        <input type="text" name="remcpf" id="remcpf" required><br>
+        <input type="text" id="remcpf" name="remcpf">
 
         <input type="submit" value="Salvar Dados">
     </form>
