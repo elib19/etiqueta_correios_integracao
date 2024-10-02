d7b78cba405b89fc06f92bdb5902f1a38a4273bd
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-02 00:01:52 -0300

    Update vendor-data-form.php

--- views/vendor-data-form.php
+++ views/vendor-data-form.php
@@ -1,33 +1,51 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+?>
+
 <div class="wrap">
     <h2>Dados do Remetente</h2>
     <form method="post">
-        <label for="remnom">Nome:</label>
-        <input type="text" id="remnom" name="remnom" required>
-
-        <label for="remend">Endereço:</label>
-        <input type="text" id="remend" name="remend" required>
-
-        <label for="remnum">Número:</label>
-        <input type="text" id="remnum" name="remnum" required>
-
-        <label for="remcom">Complemento:</label>
-        <input type="text" id="remcom" name="remcom">
-
-        <label for="rembai">Bairro:</label>
-        <input type="text" id="rembai" name="rembai" required>
-
-        <label for="remcid">Cidade:</label>
-        <input type="text" id="remcid" name="remcid" required>
-
-        <label for="remest">Estado:</label>
-        <input type="text" id="remest" name="remest" required>
-
-        <label for="remcep">CEP:</label>
-        <input type="text" id="remcep" name="remcep" required>
-
-        <label for="remcpf">CPF/CNPJ:</label>
-        <input type="text" id="remcpf" name="remcpf">
-
-        <input type="submit" value="Salvar Dados">
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Nome:</th>
+                <td><input type="text" id="remnom" name="remnom" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Endereço:</th>
+                <td><input type="text" id="remend" name="remend" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Número:</th>
+                <td><input type="text" id="remnum" name="remnum" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Complemento:</th>
+                <td><input type="text" id="remcom" name="remcom" class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Bairro:</th>
+                <td><input type="text" id="rembai" name="rembai" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Cidade:</th>
+                <td><input type="text" id="remcid" name="remcid" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Estado:</th>
+                <td><input type="text" id="remest" name="remest" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">CEP:</th>
+                <td><input type="text" id="remcep" name="remcep" required class="regular-text"></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">CPF/CNPJ:</th>
+                <td><input type="text" id="remcpf" name="remcpf" class="regular-text"></td>
+            </tr>
+        </table>
+        
+        <?php submit_button( 'Salvar Dados' ); // Botão de salvar ?>
     </form>
 </div>
