1277cfe1827db13dc9b90dd4887e79a38f4b6be8
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-09 22:18:30 -0300

    Update class-correios-uninstall.php

--- includes/class-correios-uninstall.php
+++ includes/class-correios-uninstall.php
@@ -1,15 +1,17 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+    exit; // Sair se acessado diretamente
 }
 
 class Correios_Uninstall {
 
     public static function uninstall() {
-        // Remove as opções salvas ao desinstalar o plugin
+        // Remove as opções do banco de dados ao desinstalar o plugin
         delete_option('correios_wcfm_usuario');
         delete_option('correios_wcfm_cartao');
         delete_option('correios_wcfm_api_key');
+
+        // Você pode adicionar outras operações de limpeza de banco de dados aqui, se necessário
     }
 }
