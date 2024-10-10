f751b14a942675cbff13ba188a0ee8c5c4289fbc
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-09 22:09:14 -0300

    Update class-correios-settings.php

--- includes/class-correios-settings.php
+++ includes/class-correios-settings.php
@@ -1,30 +1,35 @@
 <?php
 
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 class Correios_Settings {
 
     public static function init() {
-        add_action( 'admin_menu', array( __CLASS__, 'add_menu' ) );
-        add_action( 'admin_init', array( __CLASS__, 'register_settings' ) );
+        add_action('admin_menu', array(__CLASS__, 'add_admin_menu'));
+        add_action('admin_init', array(__CLASS__, 'register_settings'));
     }
 
-    public static function add_menu() {
+    public static function add_admin_menu() {
         add_menu_page(
-            'Configurações Correios',
-            'Correios Etiquetas',
+            __('Configurações Correios', 'correios-wcfm-integration'),
+            __('Correios', 'correios-wcfm-integration'),
             'manage_options',
-            'correios-wcfm-settings',
-            array( __CLASS__, 'settings_page' ),
+            'correios-settings',
+            array(__CLASS__, 'settings_page'),
             'dashicons-admin-generic'
         );
     }
 
     public static function register_settings() {
-        register_setting( 'correios_settings_group', 'correios_user' );
-        register_setting( 'correios_settings_group', 'correios_cartao' );
-        register_setting( 'correios_settings_group', 'correios_api_key' );
+        register_setting('correios_settings_group', 'correios_wcfm_usuario');
+        register_setting('correios_settings_group', 'correios_wcfm_cartao');
+        register_setting('correios_settings_group', 'correios_wcfm_api_key');
     }
 
     public static function settings_page() {
-        include CORREIOS_WCFM_PATH . 'includes/views/correios-settings-view.php';
+        include CORREIOS_WCFM_PLUGIN_PATH . 'views/correios-admin-settings.php';
     }
 }
+
