a2422657986265689b67947578e00442d18b587a
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-10 20:23:55 -0300

    Update virtuaria-correios-wcfm.php

--- virtuaria-correios-wcfm.php
+++ virtuaria-correios-wcfm.php
@@ -8,12 +8,13 @@
  */
 
 if (!defined('ABSPATH')) {
-    exit;
+    exit; // Exit if accessed directly
 }
 
+// Inclui os hooks do WCFM
 require_once plugin_dir_path(__FILE__) . 'hooks/wcfm-hooks.php';
 
-// Função de ativação
+// Função de ativação do plugin
 function virtuaria_correios_activate() {
     add_option('virtuaria_correios_api_key', '');
     add_option('virtuaria_correios_usuario', '');
@@ -22,16 +23,17 @@ function virtuaria_correios_activate() {
 }
 register_activation_hook(__FILE__, 'virtuaria_correios_activate');
 
-// Função de desativação
+// Função de desativação do plugin
 function virtuaria_correios_deactivate() {
     flush_rewrite_rules();
 }
 register_deactivation_hook(__FILE__, 'virtuaria_correios_deactivate');
 
-// Função de desinstalação
+// Função de desinstalação do plugin
 function virtuaria_correios_uninstall() {
     delete_option('virtuaria_correios_api_key');
     delete_option('virtuaria_correios_usuario');
     delete_option('virtuaria_correios_cartao');
 }
 register_uninstall_hook(__FILE__, 'virtuaria_correios_uninstall');
+
