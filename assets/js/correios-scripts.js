e8aef733074f74edd243682e8f7233de466ebfd1
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-09 22:15:03 -0300

    Create correios-scripts.js

---
+++ assets/js/correios-scripts.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    // Scripts customizados para o plugin Correios WCFM Integration
+
+    console.log('Correios WCFM Integration ativo!');
+
+    // Se houver alguma lógica específica para JavaScript no futuro, pode ser adicionada aqui
+});
