7bf146cd7def339e97e009d7c6b8d5d2b26bd522
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-10 20:18:26 -0300

    Update wcfm-hooks.php

--- hooks/wcfm-hooks.php
+++ hooks/wcfm-hooks.php
@@ -1,5 +1,5 @@
 <?php
-// Hook para adicionar o menu ao WCFM
+// Adicionar o menu ao WCFM
 add_filter('wcfm_menus', 'virtuaria_correios_wcfm_menu', 50);
 function virtuaria_correios_wcfm_menu($menus) {
     if (wcfm_is_vendor()) {
@@ -13,20 +13,19 @@ function virtuaria_correios_wcfm_menu($menus) {
     return $menus;
 }
 
-// Adiciona o endpoint para a página de geração de etiquetas
+// Endpoint para a página de geração de etiquetas
 add_action('init', 'virtuaria_add_wcfm_endpoints');
 function virtuaria_add_wcfm_endpoints() {
     add_rewrite_endpoint('virtuaria-etiquetas', EP_PAGES);
-    flush_rewrite_rules();
 }
 
 // Renderiza a página de geração de etiquetas no painel do vendedor
-add_action('template_redirect', 'virtuaria_wcfm_template_redirect');
-function virtuaria_wcfm_template_redirect() {
-    global $wp_query;
+add_action('wcfm_load_views', 'virtuaria_wcfm_etiquetas_page');
+function virtuaria_wcfm_etiquetas_page($view) {
+    global $WCFM, $wp_query;
 
     if (isset($wp_query->query_vars['virtuaria-etiquetas'])) {
-        require_once plugin_dir_path(__FILE__) . '../views/virtuaria-wcfm-view.php';
-        exit;
+        $WCFM->template->get_template('virtuaria-wcfm-view.php', array());
     }
 }
+?>
