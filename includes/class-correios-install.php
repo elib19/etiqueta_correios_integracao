df4f50b00a2753949d1cbba7b35372401e0d5351
elib19 <143918479+elib19@users.noreply.github.com>
2024-10-09 22:18:54 -0300

    Update class-correios-install.php

--- includes/class-correios-install.php
+++ includes/class-correios-install.php
@@ -1,15 +1,25 @@
 <?php
 
 if (!defined('ABSPATH')) {
-    exit;
+    exit; // Sair se acessado diretamente
 }
 
 class Correios_Install {
 
     public static function install() {
-        // Aqui você pode adicionar as opções padrão para o plugin
-        add_option('correios_wcfm_usuario', '');
-        add_option('correios_wcfm_cartao', '');
-        add_option('correios_wcfm_api_key', '');
+        // Adiciona as opções padrão para o plugin
+        if ( ! get_option('correios_wcfm_usuario') ) {
+            add_option('correios_wcfm_usuario', '');
+        }
+
+        if ( ! get_option('correios_wcfm_cartao') ) {
+            add_option('correios_wcfm_cartao', '');
+        }
+
+        if ( ! get_option('correios_wcfm_api_key') ) {
+            add_option('correios_wcfm_api_key', '');
+        }
+
+        // Você pode adicionar qualquer outra configuração ou operação que precise ser feita na instalação do plugin
     }
 }
